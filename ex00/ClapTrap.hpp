e5d0cd4346a335870abeeba40034b85bb38d07ab
okbrandon <okbrandonlooksdumb@gmail.com>
2023-08-31 11:59:58 +0200

    :sparkles: Finished ex00 (ClapTrap)

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 09:21:50 by bsoubaig          #+#    #+#             */
+/*   Updated: 2023/08/31 10:36:44 by bsoubaig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+# define C_RESET 	"\x1b[0m"
+# define C_RED		"\x1b[31m\x1b[1m"
+# define C_GRY		"\x1b[90m\x1b[1m"
+# define C_GRN		"\x1b[32m\x1b[1m"
+
+class ClapTrap {
+
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap(void);
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 09:21:24 by bsoubaig          #+#    #+#             */
+/*   Updated: 2023/08/31 10:41:39 by bsoubaig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void) {
+	ClapTrap	pedro("Pedro");
+	ClapTrap	alexandre("Alexandre");
+
+	/** Pedro attacks Alexandre */
+	std::cout << C_GRY "Pedro attacks Alexandre" C_RESET << std::endl;
+	pedro.attack("Alexandre");
+	alexandre.takeDamage(5);
+	/** Alexandre's healing */
+	std::cout << C_GRY "Alexandre's healing" C_RESET << std::endl;
+	alexandre.beRepaired(10);
+	/** Alexandre attacks Pedro */
+	std::cout << C_GRY "Alexandre attacks Pedro" C_RESET << std::endl;
+	alexandre.attack("Pedro");
+	pedro.takeDamage(5);
+	/** Pedro's emptying all his energy */
+	std::cout << C_GRY "Pedro's angry and emptying his energy" C_RESET << std::endl;
+	for (int i = 0; i < 9; i++) {
+		pedro.attack("Alexandre");
+		alexandre.takeDamage(1);
+	}
+	/** Pedro is trying to kill Alexandre */
+	std::cout << C_GRY "Pedro tries to finish off Alexandre" C_RESET << std::endl;
+	pedro.attack("Alexandre");
+	/** Alexandre finishes off Pedro */
+	std::cout << C_GRY "Alexandre finishes off Pedro" C_RESET << std::endl;
+	alexandre.attack("Pedro");
+	pedro.takeDamage(20);
+
+	std::cout << C_GRY "Battle is over!" C_RESET << std::endl;
+	return (0);
+}
