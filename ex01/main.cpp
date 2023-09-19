a85b2dd2c6e6781987818c22dd61d03e2e473f62
okbrandon <okbrandonlooksdumb@gmail.com>
2023-09-19 14:22:35 +0200

    :rotating_light: Running more tests (ex01)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 13:09:39 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/09/14 12:07:35 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2023/09/19 14:20:07 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 int	main(void) {
 	Animal	*animals[4];
 
-	std::cout << C_RED "Filling array of Animal objects..." C_RESET << std::endl;
+	std::cout << C_RED "\n>>> FILLING ARRAY SEMI DOG, SEMI CAT\n" C_RESET << std::endl;
 
 	for (int i = 0; i < 4; i++) {
 		if (i < 2)
@@ -33,34 +33,51 @@ int	main(void) {
 			animals[i] = new Cat();
 	}
 
-	std::cout << C_RED "Filling done. Deleting every Animal..." C_RESET << std::endl;
+	std::cout << C_GRY "Filling done. Deleting every Animal..." C_RESET << std::endl;
 
 	for (int i = 0; i < 4; i++)
 		delete animals[i];
-	
-	std::cout << C_GRY "-----------------------------------" C_RESET << std::endl;
 	// -----------------------------------
-	std::cout << C_RED "Deep copying Dog class using copy constructor..." C_RESET << std::endl;
-	Dog *dogA = new Dog();
-	Dog *dogB = new Dog(*dogA);
-	
-	(void) dogB;
+	std::cout << C_RED << "\n>>> DEEP COPY TESTING\n" << C_RESET << std::endl;
+	std::cout << C_GRY << "--- Dog's behavior ---" << C_RESET << std::endl;
+	Dog	dogA;
+	Dog dogB;
 
-	delete dogA;
-	delete dogB;
+	std::cout << C_GRY << "-------" << C_RESET << std::endl;
+	dogA.getBrain()->ideas[0] = "DogA's idea";
+	dogB.getBrain()->ideas[0] = "DogB's idea";
+	std::cout << "DogA's address (" << &dogA << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << dogA.getBrain()->ideas[0] << C_RESET << " (" << &dogA.getBrain()->ideas[0] << ")" << std::endl;
+	std::cout << "DogB's address (" << &dogB << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << dogB.getBrain()->ideas[0] << C_RESET <<" (" << &dogB.getBrain()->ideas[0] << ")" << std::endl;
 
-	std::cout << C_RED "Deep copying Cat class using copy constructor..." C_RESET << std::endl;
-	Cat *catA = new Cat();
-	Cat *catB = new Cat(*catA);
-	
-	(void) catB;
+	std::cout << C_GRY << "-------" << C_RESET << std::endl;
+	dogB = dogA;
+	std::cout << "DogA's address (" << &dogA << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << dogA.getBrain()->ideas[0] << C_RESET << " (" << &dogA.getBrain()->ideas[0] << ")" << std::endl;
+	std::cout << "DogB's address (" << &dogB << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << dogB.getBrain()->ideas[0] << C_RESET <<" (" << &dogB.getBrain()->ideas[0] << ")" << std::endl;
 
-	delete catA;
-	delete catB;
-	
-	std::cout << C_GRY "-----------------------------------" C_RESET << std::endl;
+	std::cout << C_GRY << "--- Cat's behavior ---" << C_RESET << std::endl;
+	Cat	catA;
+	Cat catB;
+
+	std::cout << C_GRY << "-------" << C_RESET << std::endl;
+	catA.getBrain()->ideas[0] = "CatA's idea";
+	catB.getBrain()->ideas[0] = "CatB's idea";
+	std::cout << "CatA's address (" << &catA << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << catA.getBrain()->ideas[0] << C_RESET << " (" << &catA.getBrain()->ideas[0] << ")" << std::endl;
+	std::cout << "CatB's address (" << &catB << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << catB.getBrain()->ideas[0] << C_RESET <<" (" << &catB.getBrain()->ideas[0] << ")" << std::endl;
+
+	std::cout << C_GRY << "-------" << C_RESET << std::endl;
+	catB = catA;
+	std::cout << "CatA's address (" << &catA << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << catA.getBrain()->ideas[0] << C_RESET << " (" << &catA.getBrain()->ideas[0] << ")" << std::endl;
+	std::cout << "CatB's address (" << &catB << ")..." << std::endl;
+	std::cout << "    idea[0]=" << C_GRN << catB.getBrain()->ideas[0] << C_RESET <<" (" << &catB.getBrain()->ideas[0] << ")" << std::endl;
 	// -----------------------------------
-	std::cout << C_RED "Running test from subject..." C_RESET << std::endl;
+	std::cout << C_RED "\n>>> RUNNING TESTS FROM SUBJECT\n" C_RESET << std::endl;
 	const Animal*	j = new Dog();
 	const Animal*	i = new Cat();
 	
