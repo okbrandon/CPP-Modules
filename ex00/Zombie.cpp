5fd81b19aedc2ab7bf1af386fff6c0ea7b5efabf
okbrandon <okbrandonlooksdumb@gmail.com>
2023-09-25 11:55:30 +0200

    :ok_hand: Reviewed some code

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,12 +6,13 @@
 /*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 09:09:41 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/09/21 09:28:01 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2023/09/25 11:55:00 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+/* Constructors & Destructors */
 Zombie::Zombie(std::string name) {
 	this->_name = name;
 	std::cout << BGRN "[Zombie] " CRESET << "New object called '" BCYN << \
@@ -23,6 +24,7 @@ Zombie::~Zombie(void) {
 		this->_name << CRESET "'..." << std::endl;
 }
 
+/* Functions */
 void	Zombie::announce(void) {
 	std::cout << BYEL << this->_name << CRESET << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,12 +6,13 @@
 /*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 09:09:41 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/09/21 09:27:50 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2023/09/25 11:54:41 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+/* Constructors & Destructors */
 Zombie::Zombie(std::string name) {
 	this->_name = name;
 	std::cout << BGRN "[Zombie] " CRESET << "New object called '" BCYN << \
@@ -29,14 +30,17 @@ Zombie::~Zombie(void) {
 		this->_name << CRESET "'..." << std::endl;
 }
 
+/* Functions */
 void	Zombie::announce(void) {
 	std::cout << BYEL << this->_name << CRESET << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
+/* Getters */
 std::string	Zombie::getName(void) {
 	return (this->_name);
 }
 
+/* Setters */
 void	Zombie::setName(std::string name) {
 	this->_name = name;
 }
